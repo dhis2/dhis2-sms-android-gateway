454948b00af4d06f807ac54505f595c1f6919d69
jaime <jaime.toca.munoz@gmail.com>
2019-11-18 17:24:38 +0100

    added logs with several fixes

--- app/src/main/java/com/jaime/smsdhis2/MainActivity.java
+++ app/src/main/java/com/jaime/smsdhis2/MainActivity.java
@@ -1,19 +1,21 @@
 package com.jaime.smsdhis2;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -22,13 +24,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.jaime.smsdhis2.network.AuthGenerator;
+import com.jaime.smsdhis2.network.IncomingSMS;
+import com.jaime.smsdhis2.network.RetrofitController;
+import com.jaime.smsdhis2.network.SMSResponse;
+import com.jaime.smsdhis2.network.SmSAPI;
+
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Enumeration;
 
-public class MainActivity extends Activity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends Activity{
 
     private static String TAG = "MainActivity";
     public static final String PREFS_NAME = "DHIS2PrefsFile";
@@ -40,6 +54,7 @@ public class MainActivity extends Activity {
     EditText txtURL;
     ToggleButton toggleForward;
     TextView textIPAddress;
+    TextView tvLogs;
     private static final int PERMISSION_SEND_SMS = 123;
 
     /** Called when the activity is first created. */
@@ -67,6 +82,7 @@ public class MainActivity extends Activity {
         txtURL = findViewById(R.id.txtURL);
         toggleForward = findViewById(R.id.toggleForward);
         textIPAddress = findViewById(R.id.textIPAddress);
+        tvLogs = findViewById(R.id.textLogs);
 
         txtURL.setText(settings.getString("dhis2.url",
                 "http://android2.dhis2.org:8080/"));
@@ -76,7 +92,9 @@ public class MainActivity extends Activity {
 
         // Show IP address
         textIPAddress.setText("Listening at: http://" + getLocalIpAddress()+ ":8000/send?recipient={recipient}&content={content}");
+        IntentFilter intentFilter = new IntentFilter();
 
+        //registerReceiver(SmsReceiver, intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED"););
         btnSave.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String password = txtPassword.getText().toString();
@@ -104,8 +122,24 @@ public class MainActivity extends Activity {
         // check permission is given
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.SEND_SMS},
+                    new String[]{Manifest.permission.RECEIVE_SMS},
                     PERMISSION_SEND_SMS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_SEND_SMS: {
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    registerReceiver(new SmsReceiver(), new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+                } else {
+                    // permission denied
+                }
+                return;
+            }
         }
     }
 
@@ -170,4 +204,114 @@ public class MainActivity extends Activity {
      * inetAddress.getHostAddress().toString(); } } } } catch (SocketException
      * ex) { Log.e(TAG, ex.toString()); } return null; }
      */
+
+     class SmsReceiver extends BroadcastReceiver {
+        private static final String TAG = "SmsReceiver";
+
+        public static final String PREFS_NAME = "DHIS2PrefsFile";
+        String urlString;
+        String username;
+        String password;
+        Context context;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            try {
+
+                this.context = context;
+
+                logMessage("SMS Received");
+
+                SharedPreferences settings = context.getSharedPreferences(
+                        PREFS_NAME, 0);
+                boolean forward = settings.getBoolean("dhis2.forward", false);
+                String commands = settings.getString("dhis2.commands", "");
+
+
+                if (!forward || commands == null) {
+                    return;
+                }
+
+                // ---get the SMS message passed in---
+                Bundle bundle = intent.getExtras();
+                SmsMessage[] msgs = null;
+                if (bundle != null) {
+                    // ---retrieve the SMS message received---
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    msgs = new SmsMessage[pdus.length];
+                    for (int i = 0; i < msgs.length; i++) {
+
+                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+
+                        String command = msgs[i].getMessageBody()
+                                ;
+                        Log.d(TAG, "message before parsing=(" + command + ")");
+
+                        urlString = settings
+                                .getString("dhis2.url",
+                                        "http://yourdhis2url/api/");
+                        username = settings.getString("dhis2.username",
+                                "admin");
+                        password = settings.getString("dhis2.password",
+                                "district");
+
+                        sendSMSToDhis2Server(username,password,urlString,
+                                msgs[i].getOriginatingAddress(), msgs[i].getMessageBody());
+                    }
+                }
+
+            } catch (Exception e) {
+                logMessage("Failed to handle SMS forwarding");
+                Log.e(TAG, "Exception:" + e, e);
+            }
+        }
+
+        public void sendSMSToDhis2Server(String user, String password, String url,
+                                         String originator, String body){
+
+            RetrofitController retrofitController = new RetrofitController(url);
+            SmSAPI smsAPI = retrofitController.start();
+
+            String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+            IncomingSMS incomingSMS = new IncomingSMS(body, originator, "unknown", date, date, "1");
+
+            String logUrl = url+ "api/sms/inbound";
+            logMessage("Server Url: "+ logUrl);
+            logMessage("Sending SMS To dhis2ServerUrl: {");
+            logMessage("originator:" + originator);
+            logMessage("receivedDate:" + date);
+            logMessage("sentDate:" + date);
+            logMessage("smsEnconding: 1");
+            logMessage("body:" + body);
+
+            logMessage("}");
+
+            smsAPI.sendSMS(AuthGenerator.getAuthToken(user,password), incomingSMS).enqueue(new Callback<SMSResponse>() {
+                @Override
+                public void onResponse(Call<SMSResponse> call, Response<SMSResponse> response) {
+                    if (response.code() > 304){
+                        logMessage(response.toString());
+                        return;
+                    }
+                    logMessage("SMS Sent to the server " + response.code());
+                    logMessage("");
+                }
+
+                @Override
+                public void onFailure(Call<SMSResponse> call, Throwable t) {
+                    logMessage("Something went wrong");
+                    logMessage("");
+                }
+            });
+        }
+
+        private void logMessage(String message){
+            if (context != null && tvLogs != null){
+                String logs = tvLogs.getText().toString();
+                String finalLogs = logs +"\n" + message;
+                tvLogs.setText(finalLogs);
+            }
+        }
+    }
 }
